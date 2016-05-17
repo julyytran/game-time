347b8ba0c734f58ebbc16f2747b670bc6a52d7c5
Chelsea Johnson <cheljoh@gmail.com>
2016-05-16 23:31:49 -0600

    can move nyan cat in four rows

--- main.bundle.js
+++ main.bundle.js
@@ -48,9 +48,10 @@
 
 	$(document).ready(function () {});
 
-	var direction = "still";
 	var canvas = document.getElementById('game');
 	var context = canvas.getContext('2d');
+	var catDrawMinY = 50;
+	var catDrawMaxY = 350;
 	var nyanCat = new Cat();
 
 	function Cat() {
@@ -58,17 +59,20 @@
 	  this.width = 100;
 	  this.height = 100;
 	  this.x = 10;
-	  this.y = 50;
+	  this.y = catDrawMinY;
 	  this.context = context;
 	}
 
+	$(document).on('keydown', function (event) {
+	  if (event.keyCode === 38) {
+	    nyanCat.y = Math.max(catDrawMinY, nyanCat.y - nyanCat.height);
+	  } else if (event.keyCode === 40) {
+	    nyanCat.y = Math.min(catDrawMaxY, nyanCat.y + nyanCat.height);
+	  }
+	});
+
 	Cat.prototype.draw = function () {
 	  this.context.drawImage(this.image, this.x, this.y);
-	  var kitty = this;
-	  $(document).on('keyup', function (e) {
-	    getDirection(e);
-	    move(kitty, direction);
-	  });
 	  return this;
 	};
 
@@ -77,40 +81,6 @@
 	  nyanCat.draw();
 	  requestAnimationFrame(gameLoop);
 	});
-
-	function getDirection(key) {
-	  if (key.keyCode === 38) {
-	    direction = "up";
-	  } else if (key.keyCode === 40) {
-	    direction = "down";
-	  }
-	}
-
-	var moveDown = {
-	  50: 150,
-	  150: 250,
-	  250: 350,
-	  350: 350
-	};
-
-	var moveUp = {
-	  50: 50,
-	  150: 50,
-	  250: 150,
-	  350: 250
-	};
-
-	var move = function move(object, direction) {
-	  if (direction === "down") {
-	    object.y = moveDown[object.y];
-	    console.log(object.y);
-	  } else if (direction === "up") {
-	    object.y = moveUp[object.y];
-	    console.log(object.y);
-	  } else if (direction === "still") {
-	    console.log("hey");
-	  }
-	};
 
 /***/ }
 /******/ ]);
