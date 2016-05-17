5df7e875030e79a4df4964ac4e5b044aa7ae90fc
Chelsea Johnson <cheljoh@gmail.com>
2016-05-17 10:16:17 -0600

    random sushi rolls appear in 4 random rows

--- main.bundle.js
+++ main.bundle.js
@@ -53,6 +53,7 @@
 	var catDrawMinY = 50;
 	var catDrawMaxY = 350;
 	var nyanCat = new Cat();
+	var sushi = new Sushi();
 
 	function Cat() {
 	  this.image = document.getElementById("nyan-cat-image");
@@ -63,6 +64,11 @@
 	  this.context = context;
 	}
 
+	Cat.prototype.draw = function () {
+	  this.context.drawImage(this.image, this.x, this.y);
+	  return this;
+	};
+
 	$(document).on('keydown', function (event) {
 	  if (event.keyCode === 38) {
 	    nyanCat.y = Math.max(catDrawMinY, nyanCat.y - nyanCat.height);
@@ -71,7 +77,19 @@
 	  }
 	});
 
-	Cat.prototype.draw = function () {
+	function Sushi() {
+	  var rowsForSprites = [70, 170, 270, 370];
+	  var sushiImages = ['egg-roll', 'roe-roll', 'fish-roll'];
+	  this.image = document.getElementById(sushiImages[Math.floor(Math.random() * sushiImages.length)]);
+	  // this.image = document.getElementById("egg-roll");
+	  this.width = 70;
+	  this.height = 58;
+	  this.x = 600; //width of the canvas
+	  this.y = rowsForSprites[Math.floor(Math.random() * rowsForSprites.length)];
+	  this.context = context;
+	}
+
+	Sushi.prototype.draw = function () {
 	  this.context.drawImage(this.image, this.x, this.y);
 	  return this;
 	};
@@ -79,6 +97,7 @@
 	requestAnimationFrame(function gameLoop() {
 	  context.clearRect(0, 0, canvas.width, canvas.height);
 	  nyanCat.draw();
+	  sushi.draw();
 	  requestAnimationFrame(gameLoop);
 	});
 
