f8af46ff05b6816feead5ec19aad03e78b88f077
Chelsea Johnson <cheljoh@gmail.com>
2016-05-17 11:24:32 -0600

    two sushi appear on screen at same time, but two different objects

--- main.bundle.js
+++ main.bundle.js
@@ -53,7 +53,6 @@
 	var catDrawMinY = 50;
 	var catDrawMaxY = 350;
 	var nyanCat = new Cat();
-	var sushi = new Sushi();
 
 	function Cat() {
 	  this.image = document.getElementById("nyan-cat-image");
@@ -81,7 +80,6 @@
 	  var rowsForSprites = [70, 170, 270, 370];
 	  var sushiImages = ['egg-roll', 'roe-roll', 'fish-roll'];
 	  this.image = document.getElementById(sushiImages[Math.floor(Math.random() * sushiImages.length)]);
-	  // this.image = document.getElementById("egg-roll");
 	  this.width = 70;
 	  this.height = 58;
 	  this.x = 600; //width of the canvas
@@ -94,10 +92,43 @@
 	  return this;
 	};
 
+	Sushi.prototype.move = function () {
+	  this.x -= 3;
+	  return this;
+	};
+
+	var sushi = new Sushi();
+	var lastTime;
+	var counter = 0;
+	var startTime = Date.now();
+
 	requestAnimationFrame(function gameLoop() {
 	  context.clearRect(0, 0, canvas.width, canvas.height);
+
+	  var now = Date.now();
+	  // var dt = (now - lastTime) / 1000.0;
+	  // counter += dt
+	  var timeElapsed = (now - startTime) / 1000;
+	  // console.log(timeElapsed)
+	  // console.log(timeElapsed)
 	  nyanCat.draw();
-	  sushi.draw();
+	  // for (var i = 0; i < 10; i++) {
+	  // sushi.draw()
+	  // sushi.move()
+
+	  if (Math.floor(timeElapsed) === 5) {
+	    sushi.draw();
+	    sushi.move();
+	    console.log('hey!');
+	  }
+
+	  // lastTime = now;
+	  // }
+	  //first frame- draw sushi, move it for a series of frames
+	  //next frame- draw new sushi on time interval which increases with difficulty
+	  // sushi.draw()
+	  // sushi.move()
+
 	  requestAnimationFrame(gameLoop);
 	});
 
