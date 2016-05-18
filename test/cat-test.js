83c2ecddfb42fdb7cc711f21f5167a279097f829
July Tran <julyytran@gmail.com>
2016-05-18 14:27:23 -0600

    add heart and cat tests

--- test/cat-test.js
+++ test/cat-test.js
@@ -5,12 +5,14 @@ const Cat = require('../lib/cat');
 
 describe("Cat", function(){
   context("with default attributes", function() {
-    // var cat = new Cat({});
+    var cat = new Cat({context: "test"});
 
     it('should assign default values', function() {
-      // assert.equal(cat.x, 10);
-      assert.equal(1, 1);
-      // assert.equal(dingus.x, 0);
+      assert.equal(cat.x, 10);
+      assert.equal(cat.y, 50);
+      assert.equal(cat.width, 100);
+      assert.equal(cat.height, 100);
+      assert.equal(cat.context, "test");
     });
-  })
-})
+  });
+});
--- test/index.js
+++ test/index.js
@@ -1,1 +1,2 @@
 require('./cat-test');
+require('./heart-test');
