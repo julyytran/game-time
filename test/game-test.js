6a1c11b6ef157dd08828c8ec1b955c099d952c94
Chelsea Johnson <cheljoh@gmail.com>
2016-05-22 13:46:11 -0600

    added reset game test

--- test/game-test.js
+++ test/game-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Game = require('../lib/game');
+const Heart = require('../lib/heart');
 
 describe("Game", function() {
   var game = new Game();
@@ -31,6 +32,16 @@ describe("Game", function() {
       var gameTimer = 500
       var actual = game.calculateSpeed(0.07, 3, 10, gameTimer);
       assert.equal(actual, 10)
+    });
+    it("resets game variables", function() {
+      var heart1 = new Heart(500, {context: "test"});
+      var heart2 = new Heart(550, {context: "test"});
+      var heart3 = new Heart(600, {context: "test"});
+      var hearts = [heart1, heart2, heart3];
+      var variables = game.resetGame(hearts)
+      assert.equal(variables[0], 0)
+      assert.equal(variables[1], 0)
+      assert.equal(variables[2].length, 0)
     });
   });
 });
